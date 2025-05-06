43aafb4ccfa0a9fbb1f42ec2f0495f8fe035c55b
Simon Enaholo <neggiehalo@gmail.com>
2025-05-06 13:55:49 +0100

    firebasekey env

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-admin-key-new.json");
+const serviceAccount = JSON.parse(process.env.FIREBASEKEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
